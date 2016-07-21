3fb8c4e357b953b6cd435fb528ccbc72c29de587
Patrick Fitzsimmons <pat@pfitz.net>
2016-07-21 11:55:40 -0400

    adding missing files

---
+++ src/main/resources/assets/v3/components/data-table-pager.vue
@@ -0,0 +1,57 @@
+
+<style lang="scss">
+ .data-table-page-vue {
+     .pager-link {
+         border: 1px solid #999;
+         display: inline-block;
+         margin-right: 2px;
+         padding: 3px 7px 3px 7px;
+     }
+     .current-page {
+         color: #999;
+         cursor: default;
+     }
+ }
+</style>
+
+<template>
+    <div class="data-table-page-vue">
+        <a v-bind:class="{'pager-link-text': true, 'pager-link': true, 'current-page': page==1}" :href="makeLink(1)">First</a>
+        <a v-for="num in pager.surroundingPages" :href="makeLink(num)" v-bind:class="{'pager-link': true, 'current-page': num==page}">
+            {{num}}
+        </a>
+        <a v-bind:class="{'pager-link-text': true, 'pager-link': true, 'current-page': page==pager.pageCount}" :href="makeLink(pager.pageCount)">Last</a>
+
+    </div>
+</template>
+<script>
+ module.exports = {
+     props: {
+         page: 1,
+         makeLink: {
+             required: true,
+             type: Function
+         },
+         basePath: {
+             required: true,
+             type: String
+         },
+         pager: Object
+     },
+     data: function() {
+         console.log('load data table ' + this.pager, ' ', this.baseUrl);
+         return {
+             
+         };
+     },
+     methods: {
+         query: function(params) {
+
+         },
+         makeUrl: function() {
+
+         }
+     }
+     
+ }
+</script>
---
+++ src/main/resources/assets/v3/components/file-upload-target.vue
@@ -0,0 +1,37 @@
+
+
+
+<style>
+
+</style>
+
+
+<template>
+    <div>
+        <form v-bind:action="formAction"
+              class="my-dropzone st-file-dropzone dropzone"
+              id="my-awesome-dropzone">
+            
+        </form>        
+    </div>
+</template>
+
+<script>
+ module.exports = {
+     props: {
+         options: Object,
+         formAction: "/st-publisher/files/upload-file"
+     },
+     ready: function() {
+         var opts = this.options || {};
+         if (!opts.dictDefaultMessage) {
+             opts.dictDefaultMessage = "Drag one more more files here. Or click to open a file picker.";
+         }
+         this.dropzone = new Dropzone($(this.$el).find('.st-file-dropzone').get(0), opts);
+     }
+ };
+</script>
+
+
+
+
